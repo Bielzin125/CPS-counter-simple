83f5b3579970d4c4b76fbe1f8c29a17e5a3c01ea
Bielzin125 <gabrielcandidogabriel001@gmail.com>
2024-11-20 13:18:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const cps = document.querySelector(".cps");
 const contador = document.querySelector('.contador');
+const reset= document.querySelector('.reset');
 let count = 0
 cps.addEventListener('mousedown', function(){
      count += 1;
      contador.innerHTML = count;
 });
 
-document.addEventListener('keydown', function(e){
-    if(e.key == 'Backspace'){
-        count = 0;
-        contador.innerHTML = count;
-        
-    }
+reset.addEventListener('mousedown', function(){
     
+    count = 0;
+    contador.innerHTML = count;
     
 });
